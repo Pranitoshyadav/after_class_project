9d79d34cb0ece8279378b9e0d96fd45ce4db43b6
Pranitoshyadav <pramod.y2070@gmail.com>
2026-03-11 19:56:57 +0545

    Update sunny.py

---
+++ sunny.py
@@ -0,0 +1,7 @@
+a = int(input("Enter your tempreture in you place (in celcius) : "))
+
+if 20 < a < 30:
+    print("Ypou an wear light clothes")
+    
+else:
+    print("you cant wear light clothes")
