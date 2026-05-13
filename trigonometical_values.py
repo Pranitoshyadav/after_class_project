56f9960c9a7673b937aa2f188d97d614c148ab20
Pranitoshyadav <pramod.y2070@gmail.com>
2026-05-13 20:25:37 +0545

    Add files via upload

---
+++ trigonometical_values.py
@@ -0,0 +1,11 @@
+p = int(input("Enter the value for perpendicular : "))
+b = int(input("Enter the value for base : "))
+h = int(input("Ente the value for hypotenuse : "))
+
+sin = p/h
+cos = b/h
+tan = p/b
+
+print(f"Sin is {sin}")
+print(f"cos is {cos}")
+print(f"tan is {tan}")
