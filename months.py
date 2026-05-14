6e4b3b66ae95f2c3587d256220e1d628111af97f
Pranitoshyadav <pramod.y2070@gmail.com>
2026-05-14 21:31:32 +0545

    Add files via upload

---
+++ months.py
@@ -0,0 +1,19 @@
+from datetime import date, time, datetime
+
+month = date.today()
+
+print("the months of the year are : ")
+print("1. January")
+print("2. Febuarey")
+print("3. March")
+print("4. April")
+print("5. May")
+print("6. June")
+print("7. July")
+print("8. Auguest")
+print("9. September")
+print("10. October")
+print("11. November")
+print("12. December")
+
+print("Todays month in number is", month.month)
