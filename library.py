ef97455d9d18679900eba4ebdee49409fd5adafe
Pranitoshyadav <pramod.y2070@gmail.com>
2026-07-22 21:03:32 +0545

    Add files via upload

---
+++ library.py
@@ -0,0 +1,14 @@
+class laibrary:
+
+    def books(slef):
+        def __init__(slef, book_avalable):
+        self.book_avalable = book_avalable
+
+        book1 = True
+        book2 = True
+        book3 = True
+        book4 = True
+
+    def retuen_book(slef):
+        
+        
